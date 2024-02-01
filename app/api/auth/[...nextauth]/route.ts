567a8672384685977680062fdc0a40df3d6876e5
haloandfable <tylerallen@live.com>
2024-01-31 22:26:52 -0500

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ import NextAuth from 'next-auth/next';
 
 // Creating the authentication handler using NextAuth with the provided options
 const handler = NextAuth(authOptions);
+// 
 
 // Exporting the authentication handler for both GET and POST requests
 export { handler as GET, handler as POST };
